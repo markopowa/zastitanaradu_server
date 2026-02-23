99ffb456367b12261a7c2ba407a66a7d6ff5428d
Marko Vuckovic <markovuckovic1992@gmail.com>
2026-02-23 17:43:00 +0100

    downloads

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
-from django.views.static import serve
+from .views import serve_media_attachment
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -19,5 +19,5 @@ urlpatterns = [
 
 if not settings.DEBUG:
     urlpatterns += [
-        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+        re_path(r"^media/(?P<path>.*)$", serve_media_attachment),
     ]
---
+++ backend/core/views.py
@@ -0,0 +1,19 @@
+import os
+
+from django.conf import settings
+from django.http import FileResponse, Http404
+
+
+def serve_media_attachment(request, path: str):
+    root = os.path.abspath(os.path.normpath(settings.MEDIA_ROOT))
+    full_path = os.path.abspath(os.path.normpath(os.path.join(root, path)))
+    if not full_path.startswith(root) or ".." in path:
+        raise Http404("Invalid path")
+    if not os.path.isfile(full_path):
+        raise Http404("Not found")
+    filename = os.path.basename(full_path)
+    return FileResponse(
+        open(full_path, "rb"),
+        as_attachment=True,
+        filename=filename,
+    )
